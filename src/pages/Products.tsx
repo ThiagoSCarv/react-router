70a70cd0bef4f7f477f13d79d199ceaa52708874
Thiago Soares <thiagocarv4@gmail.com>
2025-05-16 10:12:25 -0300

    criando NotFound 404

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,7 +1,9 @@
 export function Products() {
-  return (
-    <div>
-      <h1>Produtos</h1>
-    </div>
-  )
+	return (
+		<div>
+			<a href="/">Voltar</a>
+
+			<h1>Produtos</h1>
+		</div>
+	);
 }
--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -4,11 +4,15 @@ import { Home } from "../pages/Home";
 
 import { Products } from "../pages/Products";
 
+import { NotFound } from "../pages/NotFound";
+
 export function AppRoutes() {
 	return (
 		<Routes>
 			<Route path="/" index element={<Home />} />
 			<Route path="products" element={<Products />} />
+
+			<Route path="*" element={<NotFound />} />
 		</Routes>
 	);
 }
