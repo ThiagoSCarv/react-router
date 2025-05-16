670024043bcea88c490ba9f687ffbb70d6bc672c
Thiago Soares <thiagocarv4@gmail.com>
2025-05-16 11:01:05 -0300

    useParams

--- src/pages/Details.tsx
+++ src/pages/Details.tsx
@@ -1,7 +1,16 @@
+import { useParams } from "react-router";
+
 export function Details() {
+	const { id } = useParams();
+
 	return (
 		<div>
+      <a href="/">Voltar</a>
 			<h1>Detalhes</h1>
+
+			<span>
+				ID do Produto: <strong>{id}</strong>
+			</span>
 		</div>
 	);
 }
