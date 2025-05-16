a535e721237131a17b7d5feaf11dd30b148c8054
Thiago Soares <thiagocarv4@gmail.com>
2025-05-16 08:21:08 -0300

    iniciando estudos sobre react-router

---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router";
+import "./main.css";
+import App from "./App.tsx";
+
+// biome-ignore lint/style/noNonNullAssertion: <explanation>
+createRoot(document.getElementById("root")!).render(
+	<StrictMode>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</StrictMode>,
+);
