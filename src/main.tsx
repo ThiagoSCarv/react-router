8f2f7740aecadb87c09af3309e3bb755085856a5
Thiago Soares <thiagocarv4@gmail.com>
2025-05-16 08:55:42 -0300

    criando rota home

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import "./main.css";
-import App from "./App.tsx";
+import { AppRoutes } from "./routes/AppRoutes";
 
 // biome-ignore lint/style/noNonNullAssertion: <explanation>
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
 		<BrowserRouter>
-			<App />
+			<AppRoutes />
 		</BrowserRouter>
 	</StrictMode>,
 );
